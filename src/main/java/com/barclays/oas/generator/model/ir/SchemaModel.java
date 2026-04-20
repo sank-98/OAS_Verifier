9ef8cb94e9122ed2984e59b136979204115ac1c9
Sanket Patil <159638374+sank-98@users.noreply.github.com>
2026-04-20 21:00:01 +0530

    Create SchemaModel.java with defined fields and Lombok annotations

---
+++ src/main/java/com/barclays/oas/generator/model/ir/SchemaModel.java
@@ -0,0 +1,18 @@
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class SchemaModel {
+    private String name;
+    private SchemaType type;
+    private List<FieldModel> fields;
+    private List<String> enumValues;
+    private String description;
+    private boolean deprecated;
+}
